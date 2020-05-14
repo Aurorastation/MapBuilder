c4166f917807e8b900fd2c496bad82d174382b24
Werner <Arrow768@users.noreply.github.com>
2020-05-14 22:51:12 +0200

    Update mapServer.py

--- mapServer.py
+++ mapServer.py
@@ -109,7 +109,7 @@ def handle_generation(fullname, remote, branch = None):
         maps = glob.glob(os.path.join(repo.working_tree_dir, "maps", "**", "*.dmm"))
         args = [os.path.abspath(get_dmmtools()), "minimap", "--disable", "icon-smoothing,fancy-layers,hide-space"]
         for m in maps:
-            if m.contains("dungeon_spawns"):
+            if m.find("dungeon_spawns") != -1:
                 logger.debug("Skipping file as its within dungeon_spawns: {}".format(m))
                 continue
             a = []
